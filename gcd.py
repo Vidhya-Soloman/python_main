0222ac027c9ce0cfbaa53f1d6e51d9583225b0bd
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2023-12-27 13:41:32 +0530

    Add files via upload

--- gcd.py
+++ gcd.py
@@ -1,7 +1,7 @@
-#IMPORTING BUILTIN MATH FUNCTION 
-def gcd():
- import math
- n1=int(input("Enter first number: "))
- n2=int(input("Enter second number: "))
- print("GCD of given number is",math.gcd(n1,n2))
-gcd()
+#CO1
+#QN 7
+#GCD OF 2 NUMBERS
+import math
+n1=int(input("enter first number"))
+n2=int(input("enter second number"))
+print(math.gcd(n1,n2))
