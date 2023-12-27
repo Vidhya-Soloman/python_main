1df5e5314b082b21b1b27a944fdcb1e2a49e973e
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2023-12-27 21:14:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-#IMPORTING
 from graphics import circle
-from graphics._3dgraphics import cuboid
-from graphics import *
+#from graphics._3dgGraphics import cuboid
+from graphics import rectangle
 from graphics._3dgraphics import *
+l=float(input("Enter the length: "))
+b=float(input("Enter the breadth: "))
+rectangle.area(l,b)
+rectangle.perimeter(l,b)
+r=float(input("Enter the radius: "))
+circle.area(r)
+circle.perimeter(r)
+r=float(input("Enter the radius for sphere: "))
+sphere.area(r)
+sphere.perimeter(r)
+l=float(input("Enter the length o cuboid:"))
+b=float(input("Enter the breadth of cuboid:"))
+h=float(input("Enter the length of cuboid:"))
+cuboid.area(l,b,h)
+cuboid.perimeter(l,b,h)
