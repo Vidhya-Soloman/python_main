14a780c3f5f73bf0eac1ab01c6543f18cf135085
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2023-12-27 12:48:13 +0530

    Add files via upload

---
+++ great.py
@@ -0,0 +1,10 @@
+#greatest of 3
+a=int(input("enter first number"))
+b=int(input("enter 2nd number"))
+c=int(input("enter 3rd number"))
+if a>b:
+ print("a is greater")
+elif b>a:
+ print("b is greater")
+else:
+ print("c is greater")
