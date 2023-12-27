5789b8e0458463d0b9529e0e2b7b6b3faa3cc96d
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2023-12-27 13:47:17 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -2,6 +2,6 @@
 #QN 7
 #GCD OF 2 NUMBERS
 import math
-n1=int(input("enter first number"))
-n2=int(input("enter second number"))
-print(math.gcd(n1,n2))
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+print("GCD= ",math.gcd(n1,n2))
