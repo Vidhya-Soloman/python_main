d415db18dc59124dc17c571bbdb713bb19542e00
Vidhya S <147298749+Vidhya-Soloman@users.noreply.github.com>
2024-01-27 11:25:51 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,18 @@
+l=[]
+n=int(input("enter a limit"))
+for i in range(0,n):
+ ele=input()  #->int for integer
+ l.append(ele)
+print(l)
+
+#list user input
+
+#dict user input
+
+d={}
+n=int(input("enter th limit"))
+for i in range(0,n):
+ keys=input()   #int->for integer
+ values=input()
+ d[keys]=values
+print(d)
